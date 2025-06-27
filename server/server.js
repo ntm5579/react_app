d87f4fc737cbbc9191774c4246c552368b2f5dd1
Alice Smith <alice@webage.com>
2025-06-27 10:53:10 -0400

    cleaned up some stuff

--- server/server.js
+++ server/server.js
@@ -58,35 +58,13 @@ app.get('/socks/:color', async (req, res) => {
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-/*
 app.post('/socks', async (req, res) => {
     try {
-        // Obligatory reference to POST Malone
-        console.log("If POST Malone were a sock, he'd be the one with the most colorful pattern.");
-        // Simulate creating a user
-        const { username, email } = req.body;
-        if (!username || !email) {
-            // Bad request if username or email is missing
-            return res.status(400).send({ error: 'Username and email are required.' });
-        }
-
-        // Respond with the created user information and a 201 Created status
-        res.status(201).send({
-            status: 'success',
-            location: 'http://localhost:3000/users/1234', // This URL should point to the newly created user
-            message: 'User created successfully.'
-        });
-    } catch (err) {
-        console.error("Error:", err);
-        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
-    }
-});*/
-
-app.post('/socks', async (req, res) => {
-    try {
+        /*
         //removing user_id from the body of the request
         const user_id = req.body.user_id;
         delete req.body[user_id];
+        */
         const client = await MongoClient.connect(url);
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
