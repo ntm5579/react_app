ce3e00165f45a63d1373bef44ca55e368a5cb664
Alice Smith <alice@webage.com>
2025-06-27 15:50:50 -0400

    finished postgres lab'

--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ import { promises as fs } from 'fs';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import pg from 'pg';
 
 dotenv.config();
+
+const { Pool } = pg;
+// PostgreSQL pool configuration
+const pool = new Pool({
+    user: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: 'postgres',
+    port: 5432,
+});
+
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 const collectionName = process.env.MONGO_DB_COLLECTION;
@@ -12,6 +24,9 @@ const collectionName = process.env.MONGO_DB_COLLECTION;
 const app = express();
 app.use(cors()); // Enable CORS for all routes
 const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 
 app.get('/socks', async (req, res) => {
@@ -35,28 +50,22 @@ app.get('/socks/:color', async (req, res) => {
 
         // Console log specific parts of the request
         //console.log("Headers:", req.headers);
-        //console.log("URL:", req.url);
+        console.log("URL:", req.url);
         //console.log("Method:", req.method);
         //console.log("Query parameters:", req.query);
 
         const data = await fs.readFile('../data/socks.json', 'utf8');
         const jsonObj = JSON.parse(data);
         //sort data to find only reds
-        let colorSocks = [];
-        for (const sock in jsonObj) {
-            if (jsonObj[sock].color.toLowerCase() === color) {
-                colorSocks.push(jsonObj[sock]);
-            }
-        }
-        res.json(colorSocks);
+        let result = JSON.filter(sock => sock.color.toLowerCase() === color);
+        res.json(result);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
     }
 });
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+
 
 app.post('/socks', async (req, res) => {
     try {
@@ -86,13 +95,29 @@ app.post('/socks/search', async (req, res) => {
         const client = await MongoClient.connect(url);
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
-        const socks = await collection.find({ "sockDetails.color": req.body.searchTerm }).toArray();
+        const regex = new RegExp(req.body.searchTerm, 'i');// Create a case-insensitive regular expression
+        const socks = await collection.find({ "sockDetails.color": regex }).toArray();
         console.log(socks);
         res.json(socks);
 
     } catch (err) {
         console.error('Error:', err);
         res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
+});
+
+app.post('/socks/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const result = await pool.query('SELECT uid FROM users WHERE username = $1 AND password = $2', [username, password]);
+        if (result.rows.length > 0) {
+            res.status(200).json({ uid: result.rows[0].uid });
+        } else {
+            res.status(401).json({ message: 'Authentication failed' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
     }
 });
 
