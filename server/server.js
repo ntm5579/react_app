19f765023c8d1faa81e21ab12c2ed1b999f4e0df
Alice Smith <alice@webage.com>
2025-06-27 10:45:25 -0400

    finished lab 8

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
 const PORT = 3000;
 
 
 app.get('/socks', async (req, res) => {
     try {
-        // Console log the entire request object
-        console.log(req);
-
-        // Console log specific parts of the request
-        console.log("Headers:", req.headers);
-        console.log("URL:", req.url);
-        console.log("Method:", req.method);
-        console.log("Query parameters:", req.query);
-
-        const data = await fs.readFile('../data/socks.json', 'utf8');
-        const jsonObj = JSON.parse(data);
-        res.json(jsonObj);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
@@ -56,6 +58,7 @@ app.get('/socks/:color', async (req, res) => {
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+/*
 app.post('/socks', async (req, res) => {
     try {
         // Obligatory reference to POST Malone
@@ -77,13 +80,54 @@ app.post('/socks', async (req, res) => {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
     }
+});*/
+
+app.post('/socks', async (req, res) => {
+    try {
+        //removing user_id from the body of the request
+        const user_id = req.body.user_id;
+        delete req.body[user_id];
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.insertOne(req.body);
+        console.log(socks);
+        res.json(socks);
+
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
 });
+
+app.post('/socks/search', async (req, res) => {
+    try {
+        // TODO: Add code that can search MongoDB based on a color value
+        // from the Search text box.
+        //console.log(req.body.searchTerm);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({ "sockDetails.color": req.body.searchTerm }).toArray();
+        console.log(socks);
+        res.json(socks);
+
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
+});
+
 
 app.delete('/socks/:id', async (req, res) => {
     try {
+        // TODO: Add code that delete a sock when its delete button is clicked.
         const { id } = req.params;
-        console.log('Deleting sock with ID:', id);
-        res.status(200).send('Sock deleted successfully');
+        console.log(id);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const resStatus = await collection.deleteOne({ _id: new ObjectId(id) });
     } catch (err) {
         console.error('Error:', err);
         res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
